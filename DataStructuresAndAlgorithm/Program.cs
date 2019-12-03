e758ea1d76ba3969c25b0d86c7588eca5262ba0d
LIzhengyv <768221774@qq.com>
2019-12-03 20:00:23 +0800

    Update Program.cs

--- DataStructuresAndAlgorithm/Program.cs
+++ DataStructuresAndAlgorithm/Program.cs
@@ -2,12 +2,31 @@
 
 namespace DataStructuresAndAlgorithm
 {
-    class MainClass
+    class Program
     {
-        public static void Main(string[] args)
+        static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Console.WriteLine("你好");
+            
+
+            for (int i = 0; i <= 100; i++)
+            {
+
+
+                for (int j = 0; j <= 100; j++)
+                {
+
+                    for (int z = 0; z <=100; z++)
+                        if ((i + j + z) == 100 && (z/3  + j * 3 + i * 5) == 100)
+                        {
+                            Console.WriteLine("公鸡买" + i + "母鸡" + j + "小鸡" + z);
+                        }
+
+
+                }
+
+            }
+            Console.ReadLine();
         }
     }
 }
+
