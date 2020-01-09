b909a153c06ab6554616c9abe14819381360c614
LIzhengyv <768221774@qq.com>
2020-01-09 19:12:32 +0800

    买鸡算法

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x;
+            int y;
+            int z;
+            for (x=1;x<=20;x++)
+            {
+                for (y = 1; y<= 33; y++)
+                {
+                    for (z=3;z<=99;z+=3)
+                    {
+                        if(5 * x + 3 * y + z / 3 == 100 &&x + y + z == 100)
+                        {
+
+                            Console.WriteLine("公鸡买"+x+"母鸡买"+y+"小鸡买"+z);
+                        }
+                    }
+                }
+            }
+            Console.ReadLine();
+           
+        }
+    }
+}
